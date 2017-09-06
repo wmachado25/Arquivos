6e31f3457a0a7d4cf7e0b61f3b6fa90e1c45358b
William <00275873>
2017-09-06 13:11:19 -0300

    Atualização

--- ufrgs_fibonacci.m
+++ ufrgs_fibonacci.m
@@ -1,9 +1,8 @@
 %Fibonacci - William Machado
 function [F] = ufrgs_fibonacci(i)
+F(1) = 1;
+F(2) = 1;
 for i=3:i
-    F(1) = 1;
-    F(2) = 1;
     F(i) = F(i-1) + F(i-2);
-    s = F(i);
 end
-disp(s)
+disp(F(i))
